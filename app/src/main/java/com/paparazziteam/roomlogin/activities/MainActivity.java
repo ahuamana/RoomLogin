399e0b2f265d1d2c51a8a6dc2b2bc561182ae6d1
Alikhan <73962071@continental.edu.pe>
2021-07-03 14:06:19 -0500

    Register completed on RoomDB

--- app/src/main/java/com/paparazziteam/roomlogin/activities/MainActivity.java
+++ app/src/main/java/com/paparazziteam/roomlogin/activities/MainActivity.java
@@ -3,14 +3,87 @@ package com.paparazziteam.roomlogin.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.paparazziteam.roomlogin.R;
+import com.paparazziteam.roomlogin.appdatabase.AppDatabase;
+import com.paparazziteam.roomlogin.interfaces.UserDao;
+import com.paparazziteam.roomlogin.models.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userId, password, name;
+    Button register;
+
+    User mUser;
+    AppDatabase mAppDatabase;
+    UserDao mUserDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userId = findViewById(R.id.userid);
+        password = findViewById(R.id.password);
+        name = findViewById(R.id.name);
+        register = findViewById(R.id.register);
+
+        mUser= new User();
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mUser.setUserId(userId.getText().toString());
+                mUser.setName(name.getText().toString());
+                mUser.setPassword(password.getText().toString());
+
+                if(validateInput(mUser))
+                {
+                    //Do inset operation
+                    mAppDatabase = AppDatabase.getUserDatabase(getApplicationContext());
+                    mUserDao = mAppDatabase.userDao();
+
+                    //start insert operation on thread, asyncronis task
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            //call register method
+                            mUserDao.registerUser(mUser);
+                            Log.e("TAG","Usuario Registrado!");
+
+                        }
+                    });
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Rellenar todos los campos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+
+    private Boolean validateInput(User user)
+    {
+        if(user.getName().isEmpty() || user.getUserId().isEmpty() || user.getPassword().isEmpty() )
+        {
+            return false;
+        }else {
+            return true;
+        }
+
+
     }
 }
--- app/src/main/java/com/paparazziteam/roomlogin/interfaces/UserDao.java
+++ app/src/main/java/com/paparazziteam/roomlogin/interfaces/UserDao.java
@@ -11,15 +11,6 @@ import java.util.List;
 
 @Dao
 public interface UserDao {
-    @Query("SELECT * FROM user")
-    List<User> getAll();
-
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
 
     @Insert
     void registerUser(User user);
